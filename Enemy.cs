c4953adee8a4c7ae22ef4af8507f7f37ac80cd32
Scratch Guy <78715794+Scratch-Guy@users.noreply.github.com>
2021-04-08 11:56:25 +0530

    AI script for making enemies in Unity 3D
    Uses NavMeshSurface, which u can download from Unity's NavMeshComponents repo which I have forked.

---
+++ Enemy.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public GameObject projectile;
+
+    public float health;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    //patrolling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attack
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Sam").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    private void Update()
+    {
+        //Check for sight and Attack Range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patrolling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInAttackRange && playerInSightRange) AttackPlayer();
+
+    }
+
+    private void Patrolling()
+    {
+        if (!walkPointSet) SearchWalkPoint ();
+
+        if (walkPointSet)
+          agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+          walkPointSet = false;  
+          
+    }
+
+    private void SearchWalkPoint()
+    {
+        // Calculate random Point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+          walkPointSet = true;
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+         Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+         rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+         rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+          
+          
+          alreadyAttacked = true;
+          Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    private void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if(health <= 0) Invoke(nameof(DestroyEnemy), 5f);
+         
+    }
+    private void DestroyEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSeleted()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+
+     private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Finish")
+        {
+           health = health - 2;
+        }
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    
+}
+
